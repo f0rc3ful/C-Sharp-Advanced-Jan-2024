12313cbd59fde464d987abd56045d435cb85bf64
marin <f0rc3ful@protonmail.com>
2024-01-16 09:27:12 +0200

    new name

--- 08. Balanced Paranthesis/Program.cs
+++ 08. Balanced Paranthesis/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
 class Program
 {
@@ -6,9 +8,64 @@ class Program
     {
         char[] chars = Console.ReadLine().ToCharArray();
         // half in stack, half in queue
-        for ()
-            { }
-        //new
-        //new2
+        Stack<int> firstHalf = new Stack<int>();
+        Queue<int> secondHalf = new Queue<int>();
+        int length = chars.Length;
+        if (length % 2 == 1)
+        {
+            length -= 1;
+        }
+        for (int i = 0; i < length / 2; i++) // if length = 8, starts from 0 and ends at 4 (0, 1, 2, 3 incl)
+        {
+            if (chars[i] == '(')
+            {
+                firstHalf.Push(1);
+            }
+            else if (chars[i] == '[')
+            {
+                firstHalf.Push(2);
+            }
+            else if (chars[i] == '{')
+            {
+                firstHalf.Push(3);
+            }
+        }
+
+        for (int j = length / 2; j < length; j++) // if length = 8, starts from 4 and ends at 7 (4, 5, 6, 7 incl)
+        {
+            if (chars[j] == ')')
+            {
+                secondHalf.Enqueue(1);
+            }
+            else if (chars[j] == ']')
+            {
+                secondHalf.Enqueue(2);
+            }
+            else if (chars[j] == '}')
+            {
+                secondHalf.Enqueue(3);
+            }
+        }
+
+        bool equal = true;
+        while (firstHalf.Count > 0)
+        {
+            int intFromStack = firstHalf.Pop();
+            int intFromQueue = secondHalf.Dequeue();
+            
+            if (intFromStack != intFromQueue)
+            {
+                equal = false;
+            }
+        }
+
+        if (equal)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
     }
 }
---
+++ 09. Simple Text Editor/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string text = "";
+        Stack<string[]> previousCommand = new Stack<string[]>();
+        Stack<string> erasedStack = new Stack<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            //if (input.Length == 1)
+            //{
+            //    input = "4 0";
+            //}
+            string[] command = input.Split(" ");
+            if (command[0] == "1")
+            {
+                previousCommand.Push(command);
+                text += command[1];
+            }
+            else if (command[0] == "2")
+            {
+                int erasedCharsCount = int.Parse(command[1]);
+                string erased = text.Substring(text.Length - erasedCharsCount, erasedCharsCount);
+                erasedStack.Push(erased);
+                previousCommand.Push(command);
+                int count = int.Parse(command[1]);
+                for (int j = 0; j < count; j++)
+                {
+                    int length = text.Length;
+                    text = text.Substring(0, length - 1);
+                }
+            }
+            else if (command[0] == "3")
+            {
+                int index = int.Parse(command[1]) - 1; // 3rd char is at 2nd index, hence the "-1"
+                if (text.Length == 0)
+                {
+                    Console.WriteLine("The string you are trying to retrieve a char from is empty.");
+                }
+                else if (index > text.Length)
+                {
+
+                }
+                else
+                {
+                    char element = text[index];
+                    Console.WriteLine(element);
+                }
+            }
+            else if (command[0] == "4")
+            {
+                string[] lastCommand = previousCommand.Pop();
+                if (lastCommand[0] == "1")
+                {
+                    int charsToErase = lastCommand[1].Length;
+                    for (int m = 0; m < charsToErase; m++)
+                    {
+                        text = text.Substring(0, text.Length - 1);
+                    }
+                }
+                else if (lastCommand[0] == "2")
+                {
+                    text += erasedStack.Pop();
+                }
+            }
+        }
+    }
+}
